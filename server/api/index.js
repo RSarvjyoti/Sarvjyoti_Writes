b0297192cf21019ef09783b3ea808502af961a12
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-12 13:01:06 +0530

    tested the route

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { connectDb } from "./config/db.js";
 import {config} from 'dotenv'
+import  userRoute  from "./routes/user.route.js";
 
 config();
 const app = express();
 const PORT = process.env.PORT || 9080
 const DB_URL = process.env.MONGO_URL
-app.get('/', (req, res) => {
-    res.send("This is home route");
-})
+
+// app routes
+
+app.use('/api', userRoute);
 
 app.listen(PORT, async () => {
     await connectDb(DB_URL)
