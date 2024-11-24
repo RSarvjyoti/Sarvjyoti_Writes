216d06d7b18d24ebf91e457628b951717c7a3fe1
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-24 22:51:23 +0530

    Added the posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,49 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { Sidebar } from "flowbite-react";
-import {HiArrowSmRight, HiUser} from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signoutSuccess } from "../redux/user/userSlice";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 const DashSidebar = () => {
+  const locaton = useLocation();
+  const [tab, setTab] = useState("");
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
 
-    const locaton = useLocation();
-    const [tab, setTab] = useState('');
-    const dispatch = useDispatch();
+  useEffect(() => {
+    const urlParms = new URLSearchParams(locaton.search);
+    const tabFromUrl = urlParms.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [locaton.search]);
 
-    useEffect(() => {
-      const urlParms = new URLSearchParams(locaton.search);
-      const tabFromUrl = urlParms.get('tab');
-      if(tabFromUrl) {
-        setTab(tabFromUrl);
-      }
-      
-    },[locaton.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await axios.post("/api/user/signout");
+      dispatch(signoutSuccess());
+    } catch (error) {
+      console.log(error.response?.data?.message || error.message);
+    }
+  };
 
-    const handleSignout = async () => {
-      try {
-        const res = await axios.post('/api/user/signout');
-        dispatch(signoutSuccess());
-      } catch (error) {
-        console.log(error.response?.data?.message || error.message);
-      }
-    };
-
-  return <Sidebar className="w-full md:w-56">
-    <Sidebar.Items>
-        <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark" as="div">
-                Profile
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : "User"}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
             </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item active={tab === "posts"} icon={HiDocumentText}>
+                Posts
+              </Sidebar.Item>
             </Link>
-            <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer" onClick={handleSignout} >
-                Sign Out
-            </Sidebar.Item>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+            as="div"
+          >
+            Sign Out
+          </Sidebar.Item>
         </Sidebar.ItemGroup>
-    </Sidebar.Items>
-  </Sidebar>;
+      </Sidebar.Items>
+    </Sidebar>
+  );
 };
 
 export default DashSidebar;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 export function Dashboard() {
   const locaton = useLocation();
-  const [tab, setTab] = useState('');
+  const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParms = new URLSearchParams(locaton.search);
-    const tabFromUrl = urlParms.get('tab');
-    if(tabFromUrl) {
+    const tabFromUrl = urlParms.get("tab");
+    if (tabFromUrl) {
       setTab(tabFromUrl);
     }
-    
-  },[locaton.search]);
+  }, [locaton.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row '>
+    <div className="min-h-screen flex flex-col md:flex-row ">
       {/* sidebar */}
       <div className="md:w-56">
         <DashSidebar />
       </div>
       {/* Profile */}
-      <div className="w-full">
-        {tab === 'profile' && <DashProfile />}
-      </div>
+      {tab === "profile" && <DashProfile />}
+      {/* Posts... */}
+      {tab === "posts" && <DashPosts />}
     </div>
-  )
+  );
 }
