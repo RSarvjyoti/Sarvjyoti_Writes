b6f3aa10f575d7d76190c3a52578faae1b26359d
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-24 20:25:56 +0530

    created post api route

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ import { connectDb } from "./config/db.js";
 import {config} from 'dotenv'
 import  userRoute  from "./routes/user.route.js";
 import authRoute from './routes/auth.route.js'
+import postRoute from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 
 
@@ -16,7 +17,8 @@ const DB_URL = process.env.MONGO_URL
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute)
+app.use('/api/user', userRoute);
+app.use('/api/post', postRoute);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
---
+++ server/api/routes/post.route.js
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import { create } from '../controllers/post.controller.js';
+
+const router = Router();
+router.post('/create',verifyToken, create);
+
+export default router;
