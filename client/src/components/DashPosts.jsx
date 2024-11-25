5ae8e49f86a9839337646ad01c081d094a563bda
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-25 17:04:16 +0530

    Show the post data on dhashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { Table } from "flowbite-react";
+import {Link} from 'react-router-dom';
 
 const DashPosts = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPost] = useState([]);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get(
+          `/api/post/getposts?userId=${currentUser._id}`
+        );
+        const data = await res.data;
+        setUserPost(data.posts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
   return (
-    <div>DashPost</div>
-  )
-}
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table>
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className='divide-y'>
+              {userPosts.map(({ _id, updatedAt, category, title, image, slug }) => (
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800' key={_id}>
+                  <Table.Cell>
+                    {new Date(updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${slug}`}>
+                    <img src={image} alt={title} className="w-20 h-10 object-cover bg-gray-500"/>
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${slug}`}>{title}</Link>
+                  </Table.Cell>
+                  <Table.Cell>{category}</Table.Cell>
+                  <Table.Cell>
+                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                  </Table.Cell>
+                  <Table.Cell >
+                    <Link  className='text-teal-500 hover:underline' to={`/update-post/${_id}`}><span>Edit</span></Link>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+    </div>
+  );
+};
 
-export default DashPosts
+export default DashPosts;
