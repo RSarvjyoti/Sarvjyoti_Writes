b7aac3e87eeed2cf24ee620f9a5808d4aba0256f
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-26 19:03:40 +0530

    Added callaction component

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Button, Spinner } from "flowbite-react";
 import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -64,6 +65,9 @@ const PostPage = () => {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+       <div className='max-w-4xl mx-auto w-full'>
+        <CallToAction />
+      </div>
     </main>
   );
 };
