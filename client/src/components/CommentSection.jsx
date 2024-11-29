8c1ede2bfbc5ce8891f596011c653e073b252e04
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-29 13:14:11 +0530

    Added functionality edit the comment

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -72,6 +72,14 @@ const CommentSection = ({ postId }) => {
     }
   };  
 
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
+
   return (
     <div className="max-w-2xl object-cover row-full">
       {currentUser ? (
@@ -138,6 +146,7 @@ const CommentSection = ({ postId }) => {
               key={comment._id}
               comment={comment}
               onLike={handleLike}
+              onEdit={handleEdit}
             />
           ))}
         </>
