50f5b7c08724326df2be8b793fc0c9b76132812e
RSarvjyoti <sarvjyoti64@gmail.com>
2024-11-30 15:48:28 +0530

    remove path module

--- server/api/index.js
+++ server/api/index.js
@@ -6,14 +6,12 @@ import authRoute from './routes/auth.route.js'
 import postRoute from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 import commentRoute from './routes/comment.route.js';
-import path from 'path';
 
 config();
 const app = express();
 const PORT = process.env.PORT || 9080
 const DB_URL = process.env.MONGO_URL
 
-const __dirname = path.resolve();
 // app routes
 app.use(express.json());
 app.use(cookieParser());
@@ -21,11 +19,6 @@ app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
 app.use('/api/post', postRoute);
 app.use('/api/comment', commentRoute);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  });  
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
